a7e1b0ce71aad8bb9645e775697d4812f8a1240b
BlockLegend001 <rosarioarcamone@gmail.com>
2025-12-17 22:12:08 +0100

    Added 1.21.11

---
+++ fabric/src/main/java/com/blocklegend001/enderdragoneggrespawn/EnderDragonEggRespawn.java
@@ -0,0 +1,11 @@
+package com.blocklegend001.enderdragoneggrespawn;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnderDragonEggRespawn implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        CommonClass.init();
+    }
+}
